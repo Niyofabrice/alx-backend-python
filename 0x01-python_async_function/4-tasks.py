beaf97de1d620d92f44f79387f078bf4381135cb
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-05 08:41:13 -0700

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Tasks
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Execute wait_random n times with a maximum delay of max_delay
+    Args:
+        n (int): Number of times to execute wait_random
+        max_delay (int): Maximum delay for wait_random
+    Returns:
+        List[float]: List of delays in the order they were completed
+    """
+    delay_time: List[float] = []
+    ordered_list: List[float] = []
+
+    for _ in range(n):
+        delay_time.append(task_wait_random(max_delay))
+
+    for completed_task in asyncio.as_completed(delay_time):
+        ordered_list.append(await completed_task)
+
+    return ordered_list
