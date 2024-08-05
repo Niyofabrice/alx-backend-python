851ab26175e605007844bbfd2c7596eac2b671c9
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-05 08:31:19 -0700

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Tasks
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Function that return asyncio.Task
+    Args:
+            max_delay: integer argument
+        Return:
+            asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
