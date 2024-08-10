2d8d7c4162d9397dfddcc7cc5741ffcf21193743
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-10 06:05:36 -0700

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function that takes two floats and returns their sum
+    """
+    return a + b
