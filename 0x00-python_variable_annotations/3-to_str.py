3a97e4eaa23b939593cde026061cc9a61f53a80d
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-10 06:22:21 -0700

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that takes a float as argument and returns a string
+    """
+    return str(n)
