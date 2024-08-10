3d8fbc5d55f19a7aff90a401cf885634e671c161
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-10 06:10:07 -0700

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python3
+"""
+Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function that takes two strings as arguments return a concat string
+    """
+    return str1 + str2
