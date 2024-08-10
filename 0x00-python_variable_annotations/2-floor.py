1bf1ae6947cc03bb9257ad41efe486b656e22db9
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-10 06:15:47 -0700

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+     Function that takes a float as argument and returns the floor of the float
+    """
+    return int(n)
