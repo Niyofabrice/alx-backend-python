2c90c90dd471036b923ed0cf9656a5d0716a6885
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-31 05:51:35 -0700

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,6 +24,10 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
     def test_access_nested_map_exception(self, nested_map, path, exception):
         """
         Function that test for exceptions on access_nested_map
