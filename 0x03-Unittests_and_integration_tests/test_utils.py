15c84f1f6912d86c5cb428cdd9de20be1152db37
niyonkuru fabrice <nf.fabrice.niyonkuru@gmail.com>
2024-08-31 06:33:01 -0700

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,3 +38,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Function that test for ex on get_json without requesting a actual HTTP
+        Args:
+            test_url: url to send request to
+            payload: payload
+            mock_get: mock object
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        res = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(res, test_payload)
+        mock_get.reset_mock()
